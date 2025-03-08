629d711084569874755e4a5de13c73258f61d306
Michael Weingartner <mweingar@gmail.com>
2025-03-08 09:41:07 -0800

    Update main.go to use interactive console input

--- main.go
+++ main.go
@@ -1,41 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
+func getValidInput(prompt string, minValue int) int {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
+
+		// Trim whitespace and convert to integer
+		input = strings.TrimSpace(input)
+		num, err := strconv.Atoi(input)
+		if err != nil || num < minValue {
+			fmt.Printf("Please enter a valid number (%d or greater)\n", minValue)
+			continue
+		}
+
+		return num
+	}
+}
+
 func main() {
-	// Check if we have the correct number of arguments
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: diceroller <number of dice> <number of faces>")
-		fmt.Println("Example: diceroller 3 6")
-		os.Exit(1)
-	}
+	// Get number of dice from user
+	numDice := getValidInput("Enter the number of dice to roll: ", 1)
 
-	// Parse number of dice
-	numDice, err := strconv.Atoi(os.Args[1])
-	if err != nil || numDice < 1 {
-		fmt.Println("Error: Number of dice must be a positive integer")
-		os.Exit(1)
-	}
-
-	// Parse number of faces
-	faces, err := strconv.Atoi(os.Args[2])
-	if err != nil || faces < 2 {
-		fmt.Println("Error: Number of faces must be an integer greater than 1")
-		os.Exit(1)
-	}
+	// Get number of faces from user
+	faces := getValidInput("Enter the number of faces on each die: ", 2)
 
 	// Initialize random seed
 	rand.Seed(time.Now().UnixNano())
 
 	// Roll the dice and keep track of sum
 	sum := 0
-	fmt.Printf("Rolling %d dice with %d faces each:\n", numDice, faces)
+	fmt.Printf("\nRolling %d dice with %d faces each:\n", numDice, faces)
 	
 	for i := 1; i <= numDice; i++ {
 		roll := rand.Intn(faces) + 1 // Generate random number between 1 and faces
